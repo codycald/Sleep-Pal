ec32a96c2d077b2f7388888f7613f6d36493e96f
Cody Caldwell <cocaldwe@gmail.com>
2015-07-26 21:31:02 -0600

    Add JBChartView to project

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '8.0'
+
+target 'Sleep Pal' do
+pod 'JBChartView'
+end
+
+target 'Sleep PalTests' do
+
+end
+
--- Sleep Pal/SleepEntryDetailViewController.swift
+++ Sleep Pal/SleepEntryDetailViewController.swift
@@ -15,8 +15,6 @@ class SleepEntryDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
 }
